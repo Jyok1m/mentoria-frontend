f0279d214da8c9f165c71939b7f1fc9b8487841f
Joachim Alexandre Jasmin <joachim.jasmin-dev@proton.me>
2025-04-26 16:25:38 +0200

    Ajout de la fonctionnalité de bouton "Get Started" et réorganisation de la mise en page de l'écran d'accueil.

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,27 @@
-import { StyleSheet, Text, View, Image } from "react-native";
+import { StyleSheet, Text, View, Image, Pressable } from "react-native";
 import { MainContainer } from "@/components/wrappers/MainContainer";
 import { Colors } from "@/constants/Colors";
 
 export default function LandingScreen() {
 	return (
 		<MainContainer style={styles.container}>
-			<View style={styles.imageContainer}>
-				<Image source={require("@/assets/images/logo_no_bg.png")} style={styles.image} resizeMode="contain" />
+			<View style={styles.subContainer}>
+				<View style={styles.imageContainer}>
+					<Image source={require("@/assets/images/logo_no_bg.png")} style={styles.image} resizeMode="contain" />
+				</View>
+				<View style={styles.textContainer}>
+					<Text style={styles.title}>MentorIA</Text>
+					<Text style={styles.subTitle}>Discover personalized learning tailored just for you !</Text>
+				</View>
 			</View>
-			<View style={styles.textContainer}>
-				<Text style={styles.title}>MentorIA</Text>
-				<Text style={styles.subTitle}>Discover personalized learning tailored just for you !</Text>
-			</View>
+			<Pressable
+				onPress={() => {
+					console.log("Button Pressed");
+				}}
+				style={styles.button}
+			>
+				<Text style={styles.buttonText}>Get Started</Text>
+			</Pressable>
 		</MainContainer>
 	);
 }
@@ -24,6 +34,12 @@ const styles = StyleSheet.create({
 	},
 
 	// Logo
+	subContainer: {
+		alignItems: "center",
+		justifyContent: "center",
+		height: "70%",
+		marginTop: -20,
+	},
 	imageContainer: {
 		width: 125,
 		aspectRatio: 1,
@@ -50,5 +66,21 @@ const styles = StyleSheet.create({
 		fontSize: 18,
 		fontWeight: 400,
 		textAlign: "center",
+	},
+
+	// CTA button
+	button: {
+		backgroundColor: Colors.primary,
+		padding: 15,
+		borderRadius: 10,
+		marginTop: 20,
+		width: "100%",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	buttonText: {
+		color: Colors.background,
+		fontSize: 18,
+		fontWeight: "bold",
 	},
 });
